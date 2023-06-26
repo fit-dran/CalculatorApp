822b6dbc3cd3a1d8f568317261eddeff1e8c22c6
Luna Gatica <adolfo.luna@virginiogomez.cl>
2023-06-26 04:43:45 -0400

    Implement calculator operations: Addition, subtraction, multiplication, and division

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -8,4 +8,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Method to perform addition
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    // Method to perform subtraction
+
+    fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    // Method to perform multiplication
+
+    fun multiply(a: Int, b: Int): Int {
+        return a * b
+    }
+
+    // Method to perform division
+
+    fun divide(a: Int, b: Int): Int {
+        return a / b
+    }
+
+
 }
