36b9c72955053282827d4a4197c1024b5ad9e3e6
Luna Gatica <adolfo.luna@virginiogomez.cl>
2023-06-26 05:22:02 -0400

    Modified to fit MVC design pattern

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -6,156 +6,180 @@ import android.widget.Button
 import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnZero: Button
-    private lateinit var btnOne: Button
-    private lateinit var btnTwo: Button
-    private lateinit var btnThree: Button
-    private lateinit var btnFour: Button
-    private lateinit var btnFive: Button
-    private lateinit var btnSix: Button
-    private lateinit var btnSeven: Button
-    private lateinit var btnEight: Button
-    private lateinit var btnNine: Button
-    private lateinit var btnClear: Button
-    private lateinit var btnDivide: Button
-    private lateinit var btnPlus: Button
-    private lateinit var btnMinus: Button
-    private lateinit var btnMultiply: Button
-    private lateinit var btnEquals: Button
     private lateinit var etCalculationInput: EditText
+    private val calculatorModel = CalculatorModel()
+    private val calculatorController = CalculatorController(calculatorModel, this)
+
+    fun appendNumber(number: Int) {
+        etCalculationInput.append(number.toString())
+    }
+
+    fun clearInput() {
+        etCalculationInput.text.clear()
+    }
+
+    fun setOperation(operation: String) {
+        etCalculationInput.append(" $operation ")
+    }
+
+    fun getFirstNumber(): Int {
+        val input = etCalculationInput.text.toString()
+        return input.substring(0, input.indexOf(" ")).toInt()
+    }
+
+    fun getSecondNumber(): Int {
+        val input = etCalculationInput.text.toString()
+        return input.substring(input.indexOf(" ") + 1).toInt()
+    }
+
+    fun getOperation(): String {
+        val input = etCalculationInput.text.toString()
+        return input.substring(input.indexOf(" ") + 1, input.lastIndexOf(" "))
+    }
+
+    fun displayResult(result: Int) {
+        etCalculationInput.setText(result.toString())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initializing the views
-        btnZero = findViewById(R.id.btnZero)
-        btnOne = findViewById(R.id.btnOne)
-        btnTwo = findViewById(R.id.btnTwo)
-        btnThree = findViewById(R.id.btnThree)
-        btnFour = findViewById(R.id.btnFour)
-        btnFive = findViewById(R.id.btnFive)
-        btnSix = findViewById(R.id.btnSix)
-        btnSeven = findViewById(R.id.btnSeven)
-        btnEight = findViewById(R.id.btnEight)
-        btnNine = findViewById(R.id.btnNine)
-        btnClear = findViewById(R.id.btnClear)
-        btnDivide = findViewById(R.id.btnDivide)
-        btnPlus = findViewById(R.id.btnPlus)
-        btnMinus = findViewById(R.id.btnMinus)
-        btnMultiply = findViewById(R.id.btnMultiply)
-        btnEquals = findViewById(R.id.btnEquals)
         etCalculationInput = findViewById(R.id.etCalculationInput)
 
-        var firstNumber = 0
-        var secondNumber = 0
-        var lastOperation = ""
-
-        // Setting the click listeners
-        btnZero.setOnClickListener { etCalculationInput.append("0") }
-        btnOne.setOnClickListener { etCalculationInput.append("1") }
-        btnTwo.setOnClickListener { etCalculationInput.append("2") }
-        btnThree.setOnClickListener { etCalculationInput.append("3") }
-        btnFour.setOnClickListener { etCalculationInput.append("4") }
-        btnFive.setOnClickListener { etCalculationInput.append("5") }
-        btnSix.setOnClickListener { etCalculationInput.append("6") }
-        btnSeven.setOnClickListener { etCalculationInput.append("7") }
-        btnEight.setOnClickListener { etCalculationInput.append("8") }
-        btnNine.setOnClickListener { etCalculationInput.append("9") }
-
-        // Clearing the input
+        val btnClear = findViewById<Button>(R.id.btnClear)
         btnClear.setOnClickListener {
-            etCalculationInput.setText("")
-            firstNumber = 0
-            secondNumber = 0
-            lastOperation = ""
+            calculatorController.onClearButtonClick()
         }
 
-        // Performing division
-
-        btnDivide.setOnClickListener {
-            if (etCalculationInput.text.toString().isNotEmpty()) {
-                firstNumber = etCalculationInput.text.toString().toInt()
-                lastOperation = "divide"
-                etCalculationInput.setText("")
-            }
+        val btnAdd = findViewById<Button>(R.id.btnPlus)
+        btnAdd.setOnClickListener {
+            calculatorController.onAddButtonClick("add")
         }
 
-        // Performing addition
-
-        btnPlus.setOnClickListener {
-            if (etCalculationInput.text.toString().isNotEmpty()) {
-                firstNumber = etCalculationInput.text.toString().toInt()
-                lastOperation = "add"
-                etCalculationInput.setText("")
-            }
+        val btnSubtract = findViewById<Button>(R.id.btnMinus)
+        btnSubtract.setOnClickListener {
+            calculatorController.onAddButtonClick("subtract")
         }
 
-        // Performing subtraction
-
-        btnMinus.setOnClickListener {
-            if (etCalculationInput.text.toString().isNotEmpty()) {
-                firstNumber = etCalculationInput.text.toString().toInt()
-                lastOperation = "subtract"
-                etCalculationInput.setText("")
-            }
-        }
-
-        // Performing multiplication
-
+        val btnMultiply = findViewById<Button>(R.id.btnMultiply)
         btnMultiply.setOnClickListener {
-            if (etCalculationInput.text.toString().isNotEmpty()) {
-                firstNumber = etCalculationInput.text.toString().toInt()
-                lastOperation = "multiply"
-                etCalculationInput.setText("")
-            }
+            calculatorController.onAddButtonClick("multiply")
         }
 
-        // Performing the calculation
+        val btnDivide = findViewById<Button>(R.id.btnDivide)
+        btnDivide.setOnClickListener {
+            calculatorController.onAddButtonClick("divide")
+        }
 
+        val btnEquals = findViewById<Button>(R.id.btnEquals)
         btnEquals.setOnClickListener {
-            if (etCalculationInput.text.toString().isNotEmpty()) {
-                secondNumber = etCalculationInput.text.toString().toInt()
-                when (lastOperation) {
-                    "add" -> {
-                        etCalculationInput.setText(add(firstNumber, secondNumber).toString())
-                    }
-                    "subtract" -> {
-                        etCalculationInput.setText(subtract(firstNumber, secondNumber).toString())
-                    }
-                    "multiply" -> {
-                        etCalculationInput.setText(multiply(firstNumber, secondNumber).toString())
-                    }
-                    "divide" -> {
-                        etCalculationInput.setText(divide(firstNumber, secondNumber).toString())
-                    }
-                }
-            }
+            calculatorController.onEqualsButtonClick()
+        }
+
+        val btn0 = findViewById<Button>(R.id.btnZero)
+        btn0.setOnClickListener {
+            calculatorController.onNumberButtonClick(0)
+        }
+
+        val btn1 = findViewById<Button>(R.id.btnOne)
+        btn1.setOnClickListener {
+            calculatorController.onNumberButtonClick(1)
+        }
+
+        val btn2 = findViewById<Button>(R.id.btnTwo)
+        btn2.setOnClickListener {
+            calculatorController.onNumberButtonClick(2)
+        }
+
+        val btnThree = findViewById<Button>(R.id.btnThree)
+        btnThree.setOnClickListener {
+            calculatorController.onNumberButtonClick(3)
+        }
+
+        val btnFour = findViewById<Button>(R.id.btnFour)
+        btnFour.setOnClickListener {
+            calculatorController.onNumberButtonClick(4)
+        }
+
+        val btnFive = findViewById<Button>(R.id.btnFive)
+        btnFive.setOnClickListener {
+            calculatorController.onNumberButtonClick(5)
+        }
+
+        val btnSix = findViewById<Button>(R.id.btnSix)
+        btnSix.setOnClickListener {
+            calculatorController.onNumberButtonClick(6)
+        }
+
+        val btnSeven = findViewById<Button>(R.id.btnSeven)
+        btnSeven.setOnClickListener {
+            calculatorController.onNumberButtonClick(7)
+        }
+
+        val btnEight = findViewById<Button>(R.id.btnEight)
+        btnEight.setOnClickListener {
+            calculatorController.onNumberButtonClick(8)
+        }
+
+        val btnNine = findViewById<Button>(R.id.btnNine)
+        btnNine.setOnClickListener {
+            calculatorController.onNumberButtonClick(9)
         }
 
 
     }
 
-    // Method to perform addition
-    private fun add(a: Int, b: Int): Int {
-        return a + b
+    class CalculatorModel {
+
+        // Method to perform addition
+        fun add(a: Int, b: Int): Int {
+            return a + b
+        }
+
+        // Method to perform subtraction
+        fun subtract(a: Int, b: Int): Int {
+            return a - b
+        }
+
+        // Method to perform multiplication
+        fun multiply(a: Int, b: Int): Int {
+            return a * b
+        }
+
+        // Method to perform division
+        fun divide(a: Int, b: Int): Int {
+            return a / b
+        }
     }
 
-    // Method to perform subtraction
+    class CalculatorController(private val model: CalculatorModel, private val view: MainActivity) {
+        fun onNumberButtonClick(number: Int) {
+            view.appendNumber(number)
+        }
 
-    private fun subtract(a: Int, b: Int): Int {
-        return a - b
+        fun onClearButtonClick() {
+            view.clearInput()
+        }
+
+        fun onAddButtonClick(operation: String) {
+            view.setOperation(operation)
+        }
+
+        fun onEqualsButtonClick() {
+            val firstNumber = view.getFirstNumber()
+            val secondNumber = view.getSecondNumber()
+
+            val result = when (view.getOperation()) {
+                "add" -> model.add(firstNumber, secondNumber)
+                "subtract" -> model.subtract(firstNumber, secondNumber)
+                "multiply" -> model.multiply(firstNumber, secondNumber)
+                "divide" -> model.divide(firstNumber, secondNumber)
+                else -> 0
+            }
+
+            view.displayResult(result)
+        }
     }
-
-    // Method to perform multiplication
-
-    private fun multiply(a: Int, b: Int): Int {
-        return a * b
-    }
-
-    // Method to perform division
-
-    private fun divide(a: Int, b: Int): Int {
-        return a / b
-    }
-
 }
+
